4c07ede69a03baf62aae1f1e35d18c619a0e27f4
Victor <fanflajok@gmail.com>
2024-07-26 01:52:10 +0300

    Add files via upload
    Цель: применить навыки создания цикла while, а так же применения операторов break и continue.
    
    
    Задача "Нули ничто, отрицание недопустимо!":
    
    Дан список чисел [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
    Нужно выписывать из этого списка только положительные числа до тех пор, пока не встретите отрицательное или не закончится список (выход за границу).
    
    Пункты задачи:
    
    Запишите исходный список в переменную my_list.
    Напишите цикл while с соответствующими задаче условиями.
    Используйте операторы прерывания/продолжения цикла в соответствии с условиями задачи.
    Примечания:
    
    Чтобы перебрать список вам понадобиться обращение по индексу, запишите в отдельную переменную начальное значение - 0.
    Чтобы не выйти за границу списка, в условии цикла while рекомендуется сравнивать текущий индекс и длину списка (функция len()).
    Оператор continue - возвращает вас к условию цикла, игнорируя код после себя, break - прерывает цикл.
    0 - не отрицательное и не положительное число.

---
+++ module_2_3.py
@@ -0,0 +1,75 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+        if my_list[0] > 0:
+                print(my_list[0])
+        if my_list[0] == 0:
+                pass
+        if my_list[0] < 0:
+                break
+        if my_list[1] > 0:
+                print(my_list[1])
+        if my_list[1] == 0:
+                pass
+        if my_list[1] < 0:
+                break
+        if my_list[2] > 0:
+                print(my_list[2])
+        if my_list[2] == 0:
+                pass
+        if my_list[2] < 0:
+                break
+        if my_list[3] > 0:
+                print(my_list[3])
+        if my_list[3] == 0:
+                pass
+        if my_list[3] < 0:
+                break
+        if my_list[4] > 0:
+                print(my_list[4])
+        if my_list[4] == 0:
+                pass
+        if my_list[4] < 0:
+                break
+        if my_list[5] > 0:
+                print(my_list[5])
+        if my_list[5] == 0:
+                pass
+        if my_list[5] < 0:
+                break
+        if my_list[6] > 0:
+                print(my_list[6])
+        if my_list[6] == 0:
+                pass
+        if my_list[6] < 0:
+                break
+        if my_list[7] > 0:
+                print(my_list[7])
+        if my_list[7] == 0:
+                pass
+        if my_list[7] < 0:
+                break
+        if my_list[8] > 0:
+                print(my_list[8])
+        if my_list[8] == 0:
+                pass
+        if my_list[8] < 0:
+                break
+        if my_list[9] > 0:
+                print(my_list[9])
+        if my_list[9] == 0:
+                pass
+        if my_list[9] < 0:
+                break
+        if my_list[10] > 0:
+                print(my_list[10])
+        if my_list[10] == 0:
+                pass
+        if my_list[10] < 0:
+                break
+        if my_list[11] > 0:
+                print(my_list[11])
+        if my_list[11] == 0:
+                pass
+        if my_list[11] < 0:
+                break
